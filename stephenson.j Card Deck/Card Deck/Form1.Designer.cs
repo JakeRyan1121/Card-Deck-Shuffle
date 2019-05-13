4cbbc1ad15fea678bf162242437ee1ee4ad872ca
Jake Stephenson <41808537+JakeRyan1121@users.noreply.github.com>
2019-05-13 06:50:38 -0400

    Add files via upload

---
+++ stephenson.j Card Deck/Card Deck/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace Card_Deck
+{
+    partial class CardDeck
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CardDeck));
+            this.lstbxItems = new System.Windows.Forms.ListBox();
+            this.btnShuffle = new System.Windows.Forms.Button();
+            this.btnDrawCard = new System.Windows.Forms.Button();
+            this.lblDiscarded = new System.Windows.Forms.Label();
+            this.lblCardList = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lstbxItems
+            // 
+            this.lstbxItems.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(179)))), ((int)(((byte)(191)))), ((int)(((byte)(204)))));
+            this.lstbxItems.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.lstbxItems.CausesValidation = false;
+            this.lstbxItems.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.lstbxItems.FormattingEnabled = true;
+            this.lstbxItems.Location = new System.Drawing.Point(12, 26);
+            this.lstbxItems.Name = "lstbxItems";
+            this.lstbxItems.Size = new System.Drawing.Size(218, 377);
+            this.lstbxItems.TabIndex = 0;
+            // 
+            // btnShuffle
+            // 
+            this.btnShuffle.Font = new System.Drawing.Font("Ebrima", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnShuffle.Location = new System.Drawing.Point(248, 26);
+            this.btnShuffle.Name = "btnShuffle";
+            this.btnShuffle.Size = new System.Drawing.Size(154, 36);
+            this.btnShuffle.TabIndex = 1;
+            this.btnShuffle.Text = "Shuffle Deck";
+            this.btnShuffle.UseVisualStyleBackColor = true;
+            this.btnShuffle.Click += new System.EventHandler(this.btnShuffle_Click);
+            // 
+            // btnDrawCard
+            // 
+            this.btnDrawCard.Font = new System.Drawing.Font("Ebrima", 9.75F, System.Drawing.FontStyle.Bold);
+            this.btnDrawCard.Location = new System.Drawing.Point(248, 68);
+            this.btnDrawCard.Name = "btnDrawCard";
+            this.btnDrawCard.Size = new System.Drawing.Size(154, 36);
+            this.btnDrawCard.TabIndex = 2;
+            this.btnDrawCard.Text = "Draw Card";
+            this.btnDrawCard.UseVisualStyleBackColor = true;
+            this.btnDrawCard.Click += new System.EventHandler(this.btnDrawCard_Click);
+            // 
+            // lblDiscarded
+            // 
+            this.lblDiscarded.AutoSize = true;
+            this.lblDiscarded.Location = new System.Drawing.Point(245, 407);
+            this.lblDiscarded.Name = "lblDiscarded";
+            this.lblDiscarded.Size = new System.Drawing.Size(0, 13);
+            this.lblDiscarded.TabIndex = 3;
+            // 
+            // lblCardList
+            // 
+            this.lblCardList.AutoSize = true;
+            this.lblCardList.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.lblCardList.Font = new System.Drawing.Font("Trebuchet MS", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCardList.Location = new System.Drawing.Point(8, 3);
+            this.lblCardList.Name = "lblCardList";
+            this.lblCardList.Size = new System.Drawing.Size(81, 20);
+            this.lblCardList.TabIndex = 4;
+            this.lblCardList.Text = "Card List:";
+            // 
+            // CardDeck
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(89)))), ((int)(((byte)(140)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(414, 426);
+            this.Controls.Add(this.lblCardList);
+            this.Controls.Add(this.lblDiscarded);
+            this.Controls.Add(this.btnDrawCard);
+            this.Controls.Add(this.btnShuffle);
+            this.Controls.Add(this.lstbxItems);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.KeyPreview = true;
+            this.Name = "CardDeck";
+            this.Text = "Card Deck";
+            this.Load += new System.EventHandler(this.CardDeck_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstbxItems;
+        private System.Windows.Forms.Button btnShuffle;
+        private System.Windows.Forms.Button btnDrawCard;
+        private System.Windows.Forms.Label lblDiscarded;
+        private System.Windows.Forms.Label lblCardList;
+    }
+}
+
---
+++ stephenson.j Card Deck/Card Deck/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Card_Deck
+{  
+    public partial class CardDeck : Form
+    {
+        string[] array = new string[] { "Ace of Clubs", "Two of Clubs", "Three of Clubs", "Four of Clubs", "Five of Clubs", "Six of Clubs", "Seven of Clubs", "Eight of Clubs", "Nine of Clubs", "Ten of Clubs", "Jack of Clubs", "Queen of Clubs", "King of Clubs", "Ace of Spades", "Two of Spades", "Three of Spades", "Four of Spades", "Five of Spades", "Six of Spades", "Seven of Spades", "Eight of Spades", "Nine of Spades", "Ten of Spades", "Jack of Spades", "Queen of Spades", "King of Spades", "Ace of Hearts", "Two of Hearts", "Three of Hearts", "Four of Hearts", "Five of Hearts", "Six of Hearts", "Seven of Hearts", "Eight of Hearts", "Nine of Hearts", "Ten of Hearts", "Jack of Hearts", "Queen of Hearts", "King of Hearts", "Ace of Diamonds", "Two of Diamonds", "Three of Diamonds", "Four of Diamonds", "Five of Diamonds", "Six of Diamonds", "Seven of Diamonds", "Eight of Diamonds", "Nine of Diamonds", "Ten of Diamonds", "Jack of Diamonds", "Queen of Diamonds", "King of Diamonds" };
+        string[] hand = new string[10];
+        int a = 0, intDisCARDed = 0;
+
+        public CardDeck()
+        {
+            InitializeComponent();
+        }
+
+        public void CardDeck_Load(object sender, EventArgs e)
+        {
+            ListBoxUpdater();
+        }
+
+        public void ListBoxUpdater()
+        {
+            lstbxItems.Items.Clear();
+            for (int i = 0; i< 52; i++)
+            {
+                lstbxItems.Items.Add(array[i]);
+            }
+        }
+        public void btnShuffle_Click(object sender, EventArgs e)
+        {
+            new Random().Shuffle(array);
+            ListBoxUpdater();
+        }
+
+        public void btnDrawCard_Click(object sender, EventArgs e)
+        {
+            string temp = array[0];
+
+            try
+            {
+                hand[a++] = array[lstbxItems.SelectedIndex];
+                array[0] = array[lstbxItems.SelectedIndex];
+                array[lstbxItems.SelectedIndex] = temp;
+            }
+            catch
+            {
+                if (lstbxItems.SelectedItem == null)
+                {
+                    MessageBox.Show("Please select a card from the list.");
+                }
+                else
+                {
+                    MessageBox.Show("You have already put 10 cards in your hand.");
+                    intDisCARDed++;
+                    lblDiscarded.Text = intDisCARDed + " Card(s) have been discarded.";
+                    
+                }
+            }
+            ListBoxUpdater();
+        }
+    }
+    public static class DoTheShuffle //Randomizer class
+    {
+        public static void Shuffle<T>(this Random rng, T[] array) //this defines a generic method. This is ideal so that anything
+        {
+            int n = array.Length;
+            while (n > 1)
+            {
+                int k = rng.Next(n--);
+                T temp = array[n];
+                array[n] = array[k];
+                array[k] = temp;
+            }
+        }
+    }
+}
